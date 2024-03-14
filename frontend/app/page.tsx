4419814befd97d5e761fdcddbfcf08c366cc0e4b
Yanis540 <yanistabellout4@gmail.com>
2024-03-14 15:08:38 +0100

    added basic frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div className="flex-1 min-h-full bg-foreground ">
+      <h1 className="text-background w-full ">hello</h1>
+   </div>
+  );
+}
---
+++ frontend/install.sh
@@ -0,0 +1,4 @@
+npx shadcn-ui@latest init
+npx shadcn-ui@latest add button
+npm i react-hook-form zod @hookform/resolvers # for zodResolver 
+npx shadcn-ui@latest add input label sheet
