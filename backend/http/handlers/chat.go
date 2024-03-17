da002b146a347d56ad09af841f3bcc79f3fc589c
Yanis540 <yanistabellout4@gmail.com>
2024-03-17 14:43:31 +0100

    [backend] : Get chat

--- backend/http/handlers/chat.go
+++ backend/http/handlers/chat.go
@@ -1,31 +1,51 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
 	"pc3r/projet/db"
+	"pc3r/projet/global"
+	"encoding/json"
 	types "pc3r/projet/http/types"
 )
 
-type GetChatBody struct {
-	Id     string `json:"id"`
-	UserId string `json:"userId"`
-}
+
 type ResponseGetChatBody struct {
 	Chat types.ChatRes `json:"chat"`
 }
 
 func GetChat(res http.ResponseWriter, req *http.Request) {
-	// var body GetChatBody
-	// err := json.NewDecoder(req.Body).Decode(&body)
-	// if (err != nil) || body.Id == "" || body.UserId == "" {
-	// 	res.WriteHeader(http.StatusUnauthorized)
-	// 	message := "Unauthorized"
-	// 	json.NewEncoder(res).Encode(types.MessageResponse{Message: message})
-	// 	return
-	// }
-	user, _ := req.Context().Value("user").(db.UserModel)
+	id := req.URL.Query().Get("id")
+	if id == ""  {
+		res.WriteHeader(http.StatusUnauthorized)
+		message := "Unauthorized"
+		json.NewEncoder(res).Encode(types.MessageResponse{Message: message})
+		return
+	}
 
-	fmt.Println("Access the chat",user)
+	// user, _ := req.Context().Value("user").(db.UserModel)
+
+	prisma,ctx:= global.GetPrisma()
+
+	chat,err := prisma.Chat.FindFirst(
+		db.Chat.ID.Equals(id),
+	).With(
+		db.Chat.Users.Fetch(),
+	).Exec(ctx)
+
+	if err!=nil {
+		res.WriteHeader(http.StatusNotFound)
+		message := "Not Found"
+		json.NewEncoder(res).Encode(types.MessageResponse{Message: message})
+		return
+	}
+	chatStructure := types.ChatRes{
+		ChatModel : chat, 
+		Users : chat.Users(), 
+	}
+	response := ResponseGetChatBody{
+		Chat:chatStructure,
+	}
+	res.WriteHeader(http.StatusCreated)
+	json.NewEncoder(res).Encode(response)
 
 }
