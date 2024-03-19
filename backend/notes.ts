c04b0cc67bedd487f6a0b93830b9ca14df373aab
Yanis540 <yanistabellout4@gmail.com>
2024-03-19 02:12:09 +0100

    added some stiff

--- backend/notes.ts
+++ backend/notes.ts
@@ -8,4 +8,9 @@ cependant dans leurs propres documentation ils disent qu'il manque pas mal de fe
 
 !   chat.go : 
 ?       -   AddUserToChat : send socket to the channel 
+!   Routes : https://api.sncf.com/v1/coverage/sncf/routes
+!   https://api.sncf.com/v1/coverage/sncf/journeys?from=admin:fr:75056&to=admin:fr:69123&datetime=20240316T210444
+?       -   Get the Places :  https://api.sncf.com/v1/coverage/sncf/places?q=paris
+?           pour récupérer le place_id et le placer 
+?       -   Get the journeys : https://api.sncf.com/v1/coverage/sncf/journeys?from=admin:fr:95176& datetime=20240316T210444
 */
