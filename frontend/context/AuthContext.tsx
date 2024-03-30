6488727c0f17a690c4c9bd70bed87bce30af219c
Yanis540 <yanistabellout4@gmail.com>
2024-03-30 01:10:18 +0100

    [frontend] : fixed the auth problem

--- frontend/context/AuthContext.tsx
+++ frontend/context/AuthContext.tsx
@@ -8,15 +8,12 @@ function AuthContext(Component: any) {
 
   return function IsAuth(props: any) {
     const { user } = useAuth();
-    useLayoutEffect(() => {
-      if (!user) {
-        return redirect("/auth/sign-in");
-      }
-    }, [user]);
+    // https://github.com/pmndrs/zustand/issues/346
 
-    if (typeof window == "undefined")
+    if (useAuth.persist.hasHydrated()== false)
       return null
     if (!user) {
+      redirect("/auth/sign-in");
       return null;
     }
 
