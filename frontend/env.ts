da52792445885643347874856a60e79ed9064388
Yanis540 <yanistabellout4@gmail.com>
2024-04-13 22:57:48 +0100

    production protocol

--- frontend/env.ts
+++ frontend/env.ts
@@ -1,2 +1,3 @@
-export const SERVER_URL= `http://${process.env.NEXT_PUBLIC_SERVER_URL}`
+const PROTOCOL = process.env.NODE_ENV==='production' ? 'https':'http'
+export const SERVER_URL= `${PROTOCOL}://${process.env.NEXT_PUBLIC_SERVER_URL}`
 export const WS_SERVER_URL= `ws://${process.env.NEXT_PUBLIC_SERVER_URL}/ws`
