85f20f33f53c6af4c297fb7891f8fa07669fd6ee
Yanis540 <yanistabellout4@gmail.com>
2024-04-14 00:47:17 +0100

    trying to fix ws errors

--- frontend/env.ts
+++ frontend/env.ts
@@ -1,3 +1,4 @@
-const PROTOCOL = process.env.NODE_ENV==='production' ? 'https':'http'
-export const SERVER_URL= `${PROTOCOL}://${process.env.NEXT_PUBLIC_SERVER_URL}`
-export const WS_SERVER_URL= `ws://${process.env.NEXT_PUBLIC_SERVER_URL}/ws`
+const HTTP_PROTOCOL = process.env.NODE_ENV==='production' ? 'https':'http'
+const WS_PROTOCOL = process.env.NODE_ENV==='production' ? 'wss':'ws'
+export const SERVER_URL= `${HTTP_PROTOCOL}://${process.env.NEXT_PUBLIC_SERVER_URL}`
+export const WS_SERVER_URL= `${WS_PROTOCOL}://${process.env.NEXT_PUBLIC_SERVER_URL}/ws`
