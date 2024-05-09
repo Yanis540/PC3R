3b26a7eebba8599dbf9a3cd7c8216c3ba63750a3
Yanis540 <yanistabellout4@gmail.com>
2024-05-09 14:41:25 +0200

    added a thing

--- frontend/app/chat/(main)/page.tsx
+++ frontend/app/chat/(main)/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { LuSend } from "react-icons/lu";
 
 interface PageProps {
 
@@ -6,8 +7,10 @@ interface PageProps {
 
 function Page({}:PageProps) {
     return (
-        <div className="">
-           <h1 className='text-white'>Page</h1>
+        <div className="flex-1 flex flex-col items-center justify-center ">
+            <div className="px-4 py-2  border border-gray-300  rounded-lg hover:bg-gray-200/10 transation-all duration-200 ">
+                <LuSend className="h-6 w-6 text-gray-300 " />
+            </div>
         </div>
     );
 };
